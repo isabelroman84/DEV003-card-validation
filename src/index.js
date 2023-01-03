054676f459518d6e1fccd8fafdf429888cd42558
isabelroman84 <isabelcristinaroman@gmail.com>
2023-01-03 07:42:13 -0500

    Funciones is valid y maskify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-import validator from './validator.js';
+//alert('Hola');
+import validator from "./validator.js";
+const form = document.getElementById("form");
+//const creditCardNumber = document.getElementById("creditCardNumber");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //console.log(form);
+}),
 
-console.log(validator);
+document.getElementById('btnValidate').addEventListener("click", function () {
+  const creditCardNumber = document.getElementById("creditCardNumber").value;
+  const enmascarar = validator.maskify(creditCardNumber);
+  const resultadoValido = validator.isValid(creditCardNumber);
+  if(resultadoValido === true) {
+    alert ('Tu tarjeta de crédito ' + enmascarar + ' es válida');
+  } else {
+    alert ('Tu tarjeta de crédito ' + enmascarar + ' no es válida');
+  }
+  //console.log(enmascarar);
+  //console.log(resultadoValido);
+});
+
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,51 @@
 const validator = {
-  // ...
+  maskify: function (creditCardNumber) {
+    //El string se debe convertir a array para que el for lo pueda recorrer y por eso usamos split()
+    creditCardNumber = creditCardNumber.split("");
+    /*El for recorre cada elemento del array y reconoce que debe poner # a todos los números excepto a los 
+cuatro últimos y por eso usamos -4 que cuenta del final al inicio*/
+    for (let i = 0; i < creditCardNumber.length - 4; i++) {
+      creditCardNumber[i] = "#";
+    }
+    //Necesitamos volver a convertir el array en string y por eso usamos join()
+    creditCardNumber = creditCardNumber.join("");
+    //console.log(creditCardNumber);
+    return creditCardNumber;
+    //return alert('El número que ingresaste es ' + enmascararNum);
+  },
+  isValid: function (creditCardNumber) {
+    let sumaTotal = 0;
+    const numeroValido = Array.from(creditCardNumber, Number);
+    //console.log(numeroValido);
+    for(let i = 1; i < numeroValido.length; i += 2) {
+      //console.log(numeroValido[i]);
+      let doblarNumeros = numeroValido[i] * 2
+      //console.log(doblarNumeros);
+      if(doblarNumeros > 9) {
+        doblarNumeros -= 9
+        //console.log(doblarNumeros);
+      }
+      sumaTotal += doblarNumeros;
+      //console.log(sumaTotal);
+    }
+    for(let i = 0; i < numeroValido.length; i += 2) {
+      //console.log(numeroValido);
+      const posicionImpar = numeroValido[i];
+      //console.log(posicionImpar);
+      sumaTotal += posicionImpar;
+    }
+    //return alert('La suma total del dígito que ingresaste es ' + sumaTotal);
+    sumaTotal;
+    //console.log(sumaTotal);
+    if(sumaTotal % 10 === 0) {
+      //return alert('true');
+      //console.log('true');
+      return true;
+    } else {
+      //console.log('false');
+      //return alert('false');
+      return false;
+    }
+  }
 };
-
 export default validator;
